2b23612bbec5fe006b5e2fbbc1b56c962edf977f
Ridhii <rids.surana@gmail.com>
2015-02-07 03:24:16 -0500

    implementing light up

--- graphics.py
+++ graphics.py
@@ -8,16 +8,23 @@ edgeList = set()
 root = Tk()
 class Animation(object):
     # Override these methods when creating your own animation
-    def mousePressed(self, event): pass
+    def mousePressed(self, event):
+        for(key, courseObj) in courseViewDict.items():
+            if((courseObj.x+20>event.x) and (courseObj.x-20<event.x)) and\
+               ((courseObj.y+20>event.y) and (courseObj.y-20<event.y)):
+               pass#courseObj.lightUp()
     def keyPressed(self, event): pass
     def timerFired(self): pass
     def init(self): pass
     def redrawAll(self):
-        for (key, courseObj) in courseViewList.items():
-            courseObj.displayCirc(self.canvas)    
+        for (key, courseObj) in courseViewDict.items():
+            courseObj.displayEdges(self.canvas)
+        for(key,courseObj) in courseViewDict.items():
+            courseObj.displayCirc(self.canvas)
+            courseObj.displayCourseNum(self.canvas)
     
     # Call app.run(width,height) to get your app started
-    def run(self, width=1000, height=1000):
+    def run(self, width=1600, height=900):
         # create the root and the canvas
         #root = Tk()
         root.title("Schedule Bot")
@@ -51,23 +58,27 @@ class Animation(object):
         # and launch the app
         root.mainloop()  # This call BLOCKS (so your program waits until you close the window!)
 
-class CourseView(Animation):
+class CourseView(object):
 
     def __init__(self, course):
-         self.x = (int(course.course_number) % 1000) + 10
-         self.y = (int(course.course_number) % 10)*40 + 100  # coordinates of center
+         self.x = (int(course.course_number) % 1000) + 10+(int(course.course_number)%100)*4
+         self.y = (int(course.course_number) % 10)*70 + 100  # coordinates of center
          self.course = course
+         self.lightUp = False;
 
     def displayCirc(self,canvas):
-        canvas.create_oval(self.x-5,self.y-5,self.x+5,self.y+5,fill="Blue")
+        canvas.create_oval(self.x-25,self.y-25,self.x+25,self.y+25,fill="Blue")
         
     def displayEdges(self, canvas):
         for prereq in self.course.requisites:
-            prereqView = courseViewList[prereq]
-            canvas.drawLine(self.x, self.y, prereqView.x, prereqView.y)
+            if (prereq in courseViewDict):
+                prereqView = courseViewDict[prereq]
+                canvas.create_line(self.x, self.y, prereqView.x, prereqView.y)
+    def displayCourseNum(self,canvas):
+        canvas.create_text(self.x,self.y,text=str(int(self.course.course_number)),font="Arial")
 
-courseViewList = {}
+courseViewDict = {}
 for (key, value) in courses.course_dictionary.items():
-    courseViewList[key] = CourseView(value)
+    courseViewDict[key] = CourseView(value)
 
 Animation().run()
