1c30ae2390ed357152c2ba74acaa4b92a8d56b34
Andrew Chen <andch43212@gmail.com>
2015-02-06 21:48:52 -0500

    changing where schedule is pulled
    first time, pulled from api and solved to object locally
    next time, pulled locally

--- tree.py
+++ tree.py
@@ -1,11 +1,23 @@
 import apis
+import os.path, pickle
 
 APP_ID = "497344e6-8142-4f80-8074-d1161133a002"
 SECRET_KEY = "8jTm1V9cvkvV_YUWMJBq7iIvSOzpD7M8fY9mTafkmnq5YmDRm6AkEYeI"
+schedule_apis_filepath = "schedule"
 
 
-schedule = apis.Scheduling(app_id=APP_ID, app_secret_key=SECRET_KEY)
-print schedule.departments(semester='S14')
+
+if (os.path.exists(schedule_apis_filepath)):
+    filehandler = open(schedule_apis_filepath, 'r')
+    schedule = pickle.load(filehandler)
+    print "loaded locally"
+else:
+    schedule = apis.Scheduling(app_id=APP_ID, app_secret_key=SECRET_KEY)
+    filehandler = open(schedule_apis_filepath, 'w')
+    pickle.dump(schedule, filehandler) 
+    print "loaded from apis"
+
+#print schedule.course(semester='S14', course_number=15112)
 
 def tree():
     return "hi"
