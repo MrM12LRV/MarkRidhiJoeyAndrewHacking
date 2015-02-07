9ce38badf16a06520698a05bf9ad35d1c00a2cf2
Andrew Chen <andch43212@gmail.com>
2015-02-07 03:38:25 -0500

    implementing the lightup on click

--- graphics.py
+++ graphics.py
@@ -12,14 +12,25 @@ class Animation(object):
         for(key, courseObj) in courseViewDict.items():
             if((courseObj.x+20>event.x) and (courseObj.x-20<event.x)) and\
                ((courseObj.y+20>event.y) and (courseObj.y-20<event.y)):
-               pass#courseObj.lightUp()
+               courseObj.lightUp()
+    
+    def mouseEntered(self, event):
+        for(key, courseObj) in courseViewDict.items():
+            if((courseObj.x+20>event.x) and (courseObj.x-20<event.x)) and\
+               ((courseObj.y+20>event.y) and (courseObj.y-20<event.y)):
+               courseObj.lightUp()
+    
     def keyPressed(self, event): pass
-    def timerFired(self): pass
+    
+    def timerFired(self):
+        self.redrawAll()
+    
     def init(self): pass
+    
     def redrawAll(self):
         for (key, courseObj) in courseViewDict.items():
             courseObj.displayEdges(self.canvas)
-        for(key,courseObj) in courseViewDict.items():
+        for (key,courseObj) in courseViewDict.items():
             courseObj.displayCirc(self.canvas)
             courseObj.displayCourseNum(self.canvas)
     
@@ -60,14 +71,22 @@ class Animation(object):
 
 class CourseView(object):
 
+    width = 25
+    height = 25
+
     def __init__(self, course):
          self.x = (int(course.course_number) % 1000) + 10+(int(course.course_number)%100)*4
-         self.y = (int(course.course_number) % 10)*70 + 100  # coordinates of center
+         self.y = (int(course.course_number) % 10)*70 + 100
+         self.x2 = self.x + CourseView.width
+         self.y2 = self.y + CourseView.height
          self.course = course
-         self.lightUp = False;
+         self.isLighted = False
 
     def displayCirc(self,canvas):
-        canvas.create_oval(self.x-25,self.y-25,self.x+25,self.y+25,fill="Blue")
+        if (self.isLighted):
+            canvas.create_oval(self.x, self.y,self.x2,self.y2,fill="Yellow")
+        else:
+            canvas.create_oval(self.x, self.y,self.x2,self.y2,fill="Blue")
         
     def displayEdges(self, canvas):
         for prereq in self.course.requisites:
@@ -76,6 +95,9 @@ class CourseView(object):
                 canvas.create_line(self.x, self.y, prereqView.x, prereqView.y)
     def displayCourseNum(self,canvas):
         canvas.create_text(self.x,self.y,text=str(int(self.course.course_number)),font="Arial")
+    
+    def lightUp(self):
+        self.isLighted = True
 
 courseViewDict = {}
 for (key, value) in courses.course_dictionary.items():
