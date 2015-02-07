2304ae7de33727561afda17ee2561540d84e752a
Andrew Chen <andch43212@gmail.com>
2015-02-06 21:35:49 -0500

    adding ScottyLabs schedule api

--- tree.py
+++ tree.py
@@ -1,2 +1,11 @@
+import apis
+
+APP_ID = "497344e6-8142-4f80-8074-d1161133a002"
+SECRET_KEY = "8jTm1V9cvkvV_YUWMJBq7iIvSOzpD7M8fY9mTafkmnq5YmDRm6AkEYeI"
+
+
+schedule = apis.Scheduling(app_id=APP_ID, app_secret_key=SECRET_KEY)
+print schedule.departments(semester='S14')
+
 def tree():
     return "hi"
